8e7a9f3e1703902e75e4ecf432a622490e2695fa
kyungseon <114800024+kookyungseon@users.noreply.github.com>
2025-09-10 22:02:46 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n, int k) {
+        
+        int n_result = n*12000;
+        int k_result = k*2000;
+        int answer = (n_result + k_result)-(n/10*2000);
+
+       
+       
+            
+    
+        
+  
+        return answer;
+    }
+}
