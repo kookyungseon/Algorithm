9c2151480630c315962f0289c060014ace02e351
kyungseon <114800024+kookyungseon@users.noreply.github.com>
2025-09-13 16:04:22 +0900

    [level 0] Title: 순서쌍의 개수, Time: 79.18 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        
+        for(int i = 1; i <= n; i++){
+            for(int j =1; j<=n/i; j++){
+                 if(i * j ==n){
+                     answer++;
+                 }
+                
+            }
+           
+        }
+        return answer;
+    }
+}
