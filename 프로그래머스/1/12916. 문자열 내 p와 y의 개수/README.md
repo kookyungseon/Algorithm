3082d2b18e17ff72ce17ecd3d04061e2b96868f6
kyungseon <114800024+kookyungseon@users.noreply.github.com>
2025-10-25 19:38:44 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.06 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/README.md
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 문자열 내 p와 y의 개수 - 12916 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12916) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12916?utm_source=chatgpt.com) 
 
 ### 성능 요약
 
-메모리: 92.2 MB, 시간: 0.06 ms
+메모리: 84.3 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 25일 22:48:57
+2025년 10월 25일 19:38:39
 
 ### 문제 설명
 
--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.java
@@ -1,30 +1,41 @@
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
-        int p_count=0;
-        int y_count=0;
+        int p_count =0;
+        int y_count =0;
         
-        for(int i=0; i<s.length(); i++){
-            
-            // if(s.charAt(i)!='p'&& s.charAt(i)!='y'){
-            //     answer = true;
-            // }
-            
-            if(s.charAt(i)=='p'||s.charAt(i)=='P'){
+        
+        // 1. 모두 대문자로 변환
+        
+        s = s.toUpperCase();
+        for(int i =0; i <s.length(); i++){
+            if(s.charAt(i) =='P'){
+                
                 p_count++;
-            }else if(s.charAt(i)=='y'||s.charAt(i)=='Y'){
+            }else if(s.charAt(i)=='Y'){
+                
                 y_count++;
             }
+            
         }
+        
         if(p_count == y_count){
             
-            answer = true;
-            
+            answer =  true;
         }else{
+            
             answer = false;
         }
+       
+//         2. p 와 같으면 p_count 증가 
+//         3. y 와 같으면 y_count 증가 
+//         4. p_count 와 y_count 비교해서 같으면 true , 다르면 false 출력
+        
+        
+        
+        
+        
 
-    
         return answer;
     }
 }
